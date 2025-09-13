ac2dd7866e6e5b35016d2569971f04d69a506535
Karlito8888 <charles.bourgault@hotmail.fr>
2025-09-13 21:15:37 +0800

    Nouveau Layout A

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className={styles.footer}>
       <p className={styles.footerText}>
-        © {new Date().getFullYear()} Garden Grove Village Z
+        © {new Date().getFullYear()} Garden Grove Village A
       </p>
     </footer>
   );
